aed9a805a2089fd2a3ebcdc8e17e5085a10e768a
Ramanathan20032 <Ramanathan20032@gmail.com>
2024-06-19 20:38:51 +0530

    updated main.jsx

--- react-list/src/main.jsx
+++ react-list/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Garage from './components/Garage.jsx'
+import Products from './components/Products.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Garage />
+    <Products/>
   </React.StrictMode>,
 )
